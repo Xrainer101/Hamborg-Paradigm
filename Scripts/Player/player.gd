d60dca421ec3930ddd3cc853ab1aeb85a4471a06
Xrainer101 <146404942+Xrainer101@users.noreply.github.com>
2025-11-13 03:00:24 -0800

    Basic movement setup
    Basic movement created + a platform to stand on.

---
+++ Scripts/Player/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+var direction: float
+@export var speed: float = 200.0
+@export var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var jump_velocity: float = -350.0
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var anim: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = 0
+	
+	direction = Input.get_axis("walk_left", "walk_right")
+	
+	if direction > 0:
+		sprite.scale.x = 1
+	elif direction < 0:
+		sprite.scale.x = -1
+	
+	if direction == 0:
+		anim.play("Idle")
+	else:
+		anim.play("Walk")
+	
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	move_and_slide()
