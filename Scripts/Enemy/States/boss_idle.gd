43af90e3569a711cef17a5ee17f7eae48c13d76b
Xrainer101 <146404942+Xrainer101@users.noreply.github.com>
2025-12-15 20:08:59 -0800

    Doors + Enemy turnaround
    Added door sprites to the transition areas so players will know where they are
    Made enemies turn around when they hit a wall.

--- Scripts/Enemy/States/boss_idle.gd
+++ Scripts/Enemy/States/boss_idle.gd
@@ -34,13 +34,13 @@ func Update(delta: float):
 		entity._update_animation("Walk")
 
 func Physics_Update(delta: float):
+	if(wall_ray_cast.is_colliding()):
+		move_direction *= -1
 	if entity:
 		entity.velocity.x = move_direction * move_speed
 
 func randomize_wander():
 	move_direction = randi_range(-1, 1)
-	if(wall_ray_cast.is_colliding()):
-		move_direction *= -1
 	wander_time = randf_range(1, 3)
 	move_speed = randi_range(0, max_move_speed)
 
